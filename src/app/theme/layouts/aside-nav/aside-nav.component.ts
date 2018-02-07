f28f327ed0dc18e4f3510506724e13d76f8acfcb
李杰 <myjobemail1@163.com>
2018-02-07 14:22:01 +0800

    albedo 模版提交

---
+++ src/app/theme/layouts/aside-nav/aside-nav.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Helpers } from '../../../helpers';
+
+declare let mLayout: any;
+@Component({
+    selector: "app-aside-nav",
+    templateUrl: "./aside-nav.component.html",
+    encapsulation: ViewEncapsulation.None,
+})
+export class AsideNavComponent implements OnInit, AfterViewInit {
+
+
+    constructor() {
+
+    }
+    ngOnInit() {
+
+    }
+    ngAfterViewInit() {
+
+        mLayout.initAside();
+        let menu = mLayout.getAsideMenu(); let item = $(menu).find('a[href="' + window.location.pathname + '"]').parent('.m-menu__item'); (<any>$(menu).data('menu')).setActiveItem(item);
+    }
+
+}
---
+++ src/app/theme/pages/self-layout-blank/snippets/pages/errors/errors-error-4/errors-error-4.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorsError4Component } from './errors-error-4.component';
+import { LayoutModule } from '../../../../../../layouts/layout.module';
+
+const routes: Routes = [
+    {
+        "path": "",
+        "component": ErrorsError4Component
+    }
+];
+@NgModule({
+    imports: [
+        CommonModule, RouterModule.forChild(routes), LayoutModule
+    ], exports: [
+        RouterModule
+    ], declarations: [
+        ErrorsError4Component
+    ]
+})
+export class ErrorsError4Module {
+
+
+
+}
